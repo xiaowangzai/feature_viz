9e88442e191aec669152b9fd37299ac27b0b4575
Ubuntu <ubuntu@ip-172-31-41-232.us-west-2.compute.internal>
2018-02-25 05:33:40 +0000

    updated some stuff

--- res_features.py
+++ res_features.py
@@ -8,38 +8,34 @@ from nets import nets_factory
 res_scope = nets_factory.arg_scopes_map['resnet_v1_50']
 N_CLASSES=10
 class MLP:
-    def __init__(self, feature_shape):
-        self.extracted_features = tf.placeholder(tf.float32, [None, feature_shape])
-        self.build(self.extracted_features)
+    def __init__(self, res_checkpoint_dir):
+        self.imgs = tf.placeholder(tf.float32, [None, None, None, None])
+        self.feature_extractor()
+        self.build()
 
         self.labels = tf.placeholder(tf.int64, [None])
         y = tf.one_hot(self.labels, depth=N_CLASSES, dtype=tf.int64)
-        
+
         loss = tf.losses.mean_squared_error(y, self.y_hat)
         opt = tf.train.AdamOptimizer()
-        self.update = opt.minimize(loss)
+        self.grads = opt.compute_gradients(loss)
+     
+        self.grads = [(g, v) for (g, v) in self.grads if self.grad_filter(block_k=4, v=v)]
+        self.update = opt.apply_gradients(self.grads)
         n_equals = tf.cast(tf.equal(tf.argmax(y, axis=1), tf.argmax(self.y_hat, axis=1)), dtype=tf.float32)
         self.accuracy = tf.reduce_mean(n_equals)
+
+        # pretrained resnet
+        restore_fn = slim.assign_from_checkpoint_fn(res_checkpoint_dir, self.res_variables_to_restore)
+        self.saver = tf.train.Saver(tf.trainable_variables())
         self.sess = tf.Session()
+        restore_fn(self.sess)
         self.sess.run(tf.global_variables_initializer())
 
-    def build(self, features):
-        l1 = tf.layers.dense(features, 200, activation=tf.nn.relu)
-        l2 = tf.layers.dense(l1, 200, activation=tf.nn.relu)
-        self.y_hat = tf.layers.dense(l2, N_CLASSES, activation=tf.nn.softmax)
-    
-    def test(self, labels, features, i):
-        accuracy = self.sess.run(self.accuracy, feed_dict={self.extracted_features: features, self.labels:labels})
-        print('iter {} test accuracy: {}'.format(i, accuracy))
+    def grad_filter(self, block_k, v):
+        return True if 'block{}'.format(block_k) in v.name or 'classifier' in v.name else False
 
-    def train(self, labels, features, i):
-        _, acc = self.sess.run([self.update, self.accuracy], feed_dict={self.extracted_features: features, self.labels: labels}) 
-        print('iter {} train accuracy: {}'.format(i, acc))
-
-class ResFeatureMap:
-    def __init__(self, checkpoint_dir):
-        self.imgs = tf.placeholder(dtype='float32', shape=(None, None, None, None))
-        
+    def feature_extractor(self):
         FeatureExtractor = faster_rcnn_resnet_v1.FasterRCNNResnet50FeatureExtractor(
             is_training=False,
             first_stage_features_stride=16,
@@ -47,43 +43,62 @@ class ResFeatureMap:
             weight_decay=0.0)
         
         preprocessed_input = FeatureExtractor.preprocess(self.imgs)
-        self.rpn_feature_map = FeatureExtractor.extract_proposal_features(preprocessed_input, scope='test_scope')
-        #self._network_fn = nets_factory.get_network_fn('resnet_v1_50', num_classes=num_classes, is_training=False)
-        #variables_to_restore = slim.get_variables_to_restore()
-        #checkpoint_path = tf.train.latest_checkpoint(checkpoint_dir)
-        #restore_fn = slim.assign_from_checkpoint_fn(checkpoint_path, variables_to_restore)
+        network_fn = nets_factory.get_network_fn('resnet_v1_50', 
+            num_classes=1000, is_training=False) # 1k b/c of pretrained weight shape
+        _, endpoints = network_fn(preprocessed_input)
+        self.features = tf.reshape(endpoints['resnet_v1_50/block4'], [tf.shape(self.imgs)[0], 8192])
+        self.res_variables_to_restore = slim.get_variables_to_restore()
+         
+    def build(self):
+        with tf.variable_scope('classifier'):
+            l1 = slim.fully_connected(self.features, 200, activation_fn=tf.nn.relu)
+            l2 = slim.fully_connected(l1, 200, activation_fn=tf.nn.relu)
+            self.y_hat = tf.layers.dense(l2, N_CLASSES, activation=tf.nn.softmax)
 
-        self.sess = tf.Session()
-        #restore_fn(self.sess)
-        self.sess.run(tf.global_variables_initializer())
-
-    def generate(self, imgs):
+    def preprocess_imgs(self, imgs): 
         transformed_imgs = []
         for i in range(len(imgs)):
-            new_img = np.reshape(imgs[i], [28, 28, 1])# [batch, height, width, channels]
-            new_img = cv2.resize(new_img, (33, 33))# gotta be atleast 33x33 b/c resnet feature extractor is a joke
-
+            new_img = np.reshape(imgs[i], [28, 28, 1])  # [batch, height, width, channels]
+            #new_img = np.repeat(new_img, 3, axis=2) # copy channel b/c mnist is grayscale
+            new_img = cv2.resize(new_img, (33, 33))  # gotta be atleast 33x33 b/c resnet feature extractor is a joke
+            new_img = 255 * new_img # mnist was represented between 0-1 but pretrained imgs 0-255
             transformed_imgs.append(new_img)
         transformed_imgs = np.reshape(transformed_imgs, [-1, 33, 33, 1])
-        learned_embeddings = self.sess.run(self.rpn_feature_map, feed_dict={self.imgs: transformed_imgs})
-        learned_embeddings = np.reshape(learned_embeddings, [len(imgs), -1])
-        return learned_embeddings 
+        return transformed_imgs
+
+    def test(self, labels, imgs, i):
+        imgs = self.preprocess_imgs(imgs)
+        y_hat, accuracy = self.sess.run([self.y_hat, self.accuracy], feed_dict={self.imgs: imgs, self.labels:labels})
+        print('iter {} test accuracy: {}'.format(i, accuracy))
+
+    def train(self, labels, imgs, i):
+        imgs = self.preprocess_imgs(imgs)
+        g, _, acc = self.sess.run([self.grads, self.update, self.accuracy], feed_dict={self.imgs: imgs, self.labels: labels}) 
+        print('iter {} train accuracy: {}'.format(i, acc))
+
+    def save(self, i):
+        self.saver.save(self.sess, 'mnist_ckpt/mnist', global_step=i)
+
+    def load(self):
+        ckpt_path = tf.train.latest_checkpoint('mnist_ckpt')
+        if ckpt_path is not None:
+            self.saver.restore(self.sess, ckpt_path) 
+        return 0 if ckpt_path is None else int(ckpt_path.split('-')[-1])
 
 def vizualize_feature():
     return
 
 mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-train_data = mnist.train.images
-train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
 
-res_feature_map = ResFeatureMap(checkpoint_dir='/home/ubuntu/feature_viz/res50')
-features = np.reshape(res_feature_map.generate([train_data[0]]), [1, -1])
-mlp = MLP(features.shape[-1])
+batch_x, batch_y = mnist.train.next_batch(1)
+mlp = MLP(res_checkpoint_dir='/home/ubuntu/feature_viz/resnet_v1_50.ckpt')
+i = mlp.load()
 batch_size = 128
-for i in range(0, train_data.shape[0], batch_size):
-    x = train_data[i:i+batch_size]
-    y = train_labels[i:i+batch_size] 
-    features = res_feature_map.generate(x)
-
-    mlp.train(y, features, i)
-
+while(mnist.train._epochs_completed < 9):
+    batch_x, batch_y = mnist.train.next_batch(batch_size)
+    mlp.train(batch_y, batch_x, i)
+    i += 1
+    if i % 10 == 0:
+        mlp.save(i)
+        print('EPOCH: ', mnist.train._epochs_completed)
+        print('saving after {} iterations'.format(i))
