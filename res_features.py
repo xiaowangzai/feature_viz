a322b214a769172a18e5c8a3efa2903db5448b21
Ubuntu <ubuntu@ip-172-31-41-232.us-west-2.compute.internal>
2018-02-25 06:06:42 +0000

    drop fasterrcnn preprocessing

--- res_features.py
+++ res_features.py
@@ -9,7 +9,7 @@ res_scope = nets_factory.arg_scopes_map['resnet_v1_50']
 N_CLASSES=10
 class MLP:
     def __init__(self, res_checkpoint_dir):
-        self.imgs = tf.placeholder(tf.float32, [None, None, None, None])
+        self.imgs = tf.placeholder(tf.float32, [None, None, None, 3])
         self.feature_extractor()
         self.build()
 
@@ -19,8 +19,7 @@ class MLP:
         loss = tf.losses.mean_squared_error(y, self.y_hat)
         opt = tf.train.AdamOptimizer()
         self.grads = opt.compute_gradients(loss)
-     
-        self.grads = [(g, v) for (g, v) in self.grads if self.grad_filter(block_k=4, v=v)]
+        self.grads = [(g, v) for (g, v) in self.grads if self.grad_filter(block_k=0, v=v)] # if 0 update everything but logits
         self.update = opt.apply_gradients(self.grads)
         n_equals = tf.cast(tf.equal(tf.argmax(y, axis=1), tf.argmax(self.y_hat, axis=1)), dtype=tf.float32)
         self.accuracy = tf.reduce_mean(n_equals)
@@ -33,19 +32,20 @@ class MLP:
         self.sess.run(tf.global_variables_initializer())
 
     def grad_filter(self, block_k, v):
-        return True if 'block{}'.format(block_k) in v.name or 'classifier' in v.name else False
+        if block_k == 0:
+            if 'logits' not in v.name:
+                return True
+        if 'classifier' in v.name:
+            return True
+        for k in range(block_k, 5, 1): # max block number is 4 
+            if 'block{}'.format(k) in v.name:
+                return True
+        return False
 
     def feature_extractor(self):
-        FeatureExtractor = faster_rcnn_resnet_v1.FasterRCNNResnet50FeatureExtractor(
-            is_training=False,
-            first_stage_features_stride=16,
-            reuse_weights=None,
-            weight_decay=0.0)
-        
-        preprocessed_input = FeatureExtractor.preprocess(self.imgs)
         network_fn = nets_factory.get_network_fn('resnet_v1_50', 
             num_classes=1000, is_training=False) # 1k b/c of pretrained weight shape
-        _, endpoints = network_fn(preprocessed_input)
+        _, endpoints = network_fn(self.imgs)
         self.features = tf.reshape(endpoints['resnet_v1_50/block4'], [tf.shape(self.imgs)[0], 8192])
         self.res_variables_to_restore = slim.get_variables_to_restore()
          
@@ -59,11 +59,11 @@ class MLP:
         transformed_imgs = []
         for i in range(len(imgs)):
             new_img = np.reshape(imgs[i], [28, 28, 1])  # [batch, height, width, channels]
-            #new_img = np.repeat(new_img, 3, axis=2) # copy channel b/c mnist is grayscale
+            new_img = np.repeat(new_img, 3, axis=2) # copy channel b/c mnist is grayscale
             new_img = cv2.resize(new_img, (33, 33))  # gotta be atleast 33x33 b/c resnet feature extractor is a joke
             new_img = 255 * new_img # mnist was represented between 0-1 but pretrained imgs 0-255
             transformed_imgs.append(new_img)
-        transformed_imgs = np.reshape(transformed_imgs, [-1, 33, 33, 1])
+        transformed_imgs = np.reshape(transformed_imgs, [-1, 33, 33, 3])
         return transformed_imgs
 
     def test(self, labels, imgs, i):
@@ -98,7 +98,7 @@ while(mnist.train._epochs_completed < 9):
     batch_x, batch_y = mnist.train.next_batch(batch_size)
     mlp.train(batch_y, batch_x, i)
     i += 1
-    if i % 10 == 0:
+    if i % 100 == 0:
         mlp.save(i)
         print('EPOCH: ', mnist.train._epochs_completed)
         print('saving after {} iterations'.format(i))
